534e8736d8ab1edde0403a7f0f488755569b8b47
manjugoutham <gouthamy219@gmail.com>
2022-01-13 17:53:04 +0530

    Ability to create a Address Book Service DB

---
+++ src/Address_Book.sql
@@ -0,0 +1,23 @@
+//UC1............
+
+mysql> create database Address_Book_DataBase;
+Query OK, 1 row affected (0.29 sec)
+
+mysql> use Address_Book_DataBase;
+Database changed
+mysql> show databases;
++-----------------------+
+| Database              |
++-----------------------+
+| address_book_database |
+| information_schema    |
+| mysampledb            |
+| mysql                 |
+| payroll_service       |
+| performance_schema    |
+| sakila                |
+| sys                   |
+| world                 |
++-----------------------+
+9 rows in set (1.69 sec)
+
