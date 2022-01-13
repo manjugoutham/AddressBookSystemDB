0e495c04465586f86283b988c618872b26256f86
manjugoutham <gouthamy219@gmail.com>
2022-01-13 18:05:57 +0530

    Ability to Retrieve Person belonging to a City or State from the Address Book

--- src/Address_Book.sql
+++ src/Address_Book.sql
@@ -1,11 +1,7 @@
-mysql> DELETE FROM Address_Book WHERE firstName = "Afzer";
-Query OK, 1 row affected (0.20 sec)
-
-mysql> select * from Address_Book;
+mysql> select * from Address_Book where state = 'Karnataka';
 +-----------+----------+----------+-------+-----------+--------+-------------+------------------+
 | firstName | lastName | address  | city  | state     | zip    | phoneNumber | email            |
 +-----------+----------+----------+-------+-----------+--------+-------------+------------------+
 | Goutham   | Y        | RT nagar | Kolar | Karnataka | 565656 |   987654321 | gouthu@gmail.com |
-| Harish    | L        | andra    | Delhi | Bihar     | 565006 |   810545761 | smith@gmail.com  |
 +-----------+----------+----------+-------+-----------+--------+-------------+------------------+
-2 rows in set (0.00 sec)
+1 row in set (0.00 sec)
