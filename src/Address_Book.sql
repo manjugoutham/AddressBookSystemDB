a059615bd28fc3b686ad8ae8ad654998d3d0bae9
manjugoutham <gouthamy219@gmail.com>
2022-01-13 18:02:45 +0530

    Ability to delete a person using person's name

--- src/Address_Book.sql
+++ src/Address_Book.sql
@@ -1,12 +1,11 @@
-mysql> UPDATE Address_Book SET city = 'Kolar' WHERE firstName = 'Goutham';
-Query OK, 1 row affected (0.18 sec)
-Rows matched: 1  Changed: 1  Warnings: 0
+mysql> DELETE FROM Address_Book WHERE firstName = "Afzer";
+Query OK, 1 row affected (0.20 sec)
 
 mysql> select * from Address_Book;
-+-----------+----------+----------+----------+-----------+--------+-------------+------------------+
-| firstName | lastName | address  | city     | state     | zip    | phoneNumber | email            |
-+-----------+----------+----------+----------+-----------+--------+-------------+------------------+
-| Goutham   | Y        | RT nagar | Kolar    | Karnataka | 565656 |   987654321 | gouthu@gmail.com |
-| Afzer     | L        | bihar    | banglore | Karnataka | 565656 |   810545761 | allen@gmail.com  |
-| Harish    | L        | andra    | Delhi    | Bihar     | 565006 |   810545761 | smith@gmail.com  |
-+-----------+----------+----------+----------+-----------+--------+-------------+------------------+
++-----------+----------+----------+-------+-----------+--------+-------------+------------------+
+| firstName | lastName | address  | city  | state     | zip    | phoneNumber | email            |
++-----------+----------+----------+-------+-----------+--------+-------------+------------------+
+| Goutham   | Y        | RT nagar | Kolar | Karnataka | 565656 |   987654321 | gouthu@gmail.com |
+| Harish    | L        | andra    | Delhi | Bihar     | 565006 |   810545761 | smith@gmail.com  |
++-----------+----------+----------+-------+-----------+--------+-------------+------------------+
+2 rows in set (0.00 sec)
