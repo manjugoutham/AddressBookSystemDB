c5128b474675b6e23fabfaf6deb2fe90cb307f5f
manjugoutham <gouthamy219@gmail.com>
2022-01-13 18:07:15 +0530

    Ability to understand the size of address book by City and State

--- src/Address_Book.sql
+++ src/Address_Book.sql
@@ -1,7 +1,15 @@
-mysql> select * from Address_Book where state = 'Karnataka';
-+-----------+----------+----------+-------+-----------+--------+-------------+------------------+
-| firstName | lastName | address  | city  | state     | zip    | phoneNumber | email            |
-+-----------+----------+----------+-------+-----------+--------+-------------+------------------+
-| Goutham   | Y        | RT nagar | Kolar | Karnataka | 565656 |   987654321 | gouthu@gmail.com |
-+-----------+----------+----------+-------+-----------+--------+-------------+------------------+
+mysql> SELECT COUNT(city) FROM Address_Book;
++-------------+
+| COUNT(city) |
++-------------+
+|           3 |
++-------------+
+1 row in set (0.02 sec)
+
+mysql> SELECT COUNT(city) FROM Address_Book WHERE state="Karnataka";
++-------------+
+| COUNT(city) |
++-------------+
+|           1 |
++-------------+
 1 row in set (0.00 sec)
