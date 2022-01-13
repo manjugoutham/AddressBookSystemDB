6049e5afa14a924ca5741653d0e4236205a7c8a1
manjugoutham <gouthamy219@gmail.com>
2022-01-13 17:56:18 +0530

    create a Address Book Table

--- src/Address_Book.sql
+++ src/Address_Book.sql
@@ -1,23 +1,17 @@
-//UC1............
+mysql>  create table Address_Book(firstName varchar(255) , lastName varchar(255) , address varchar(255),city varchar(255), state varchar(255), zip int , phoneNumber int , email varchar(255));
+Query OK, 0 rows affected (3.33 sec)
 
-mysql> create database Address_Book_DataBase;
-Query OK, 1 row affected (0.29 sec)
-
-mysql> use Address_Book_DataBase;
-Database changed
-mysql> show databases;
-+-----------------------+
-| Database              |
-+-----------------------+
-| address_book_database |
-| information_schema    |
-| mysampledb            |
-| mysql                 |
-| payroll_service       |
-| performance_schema    |
-| sakila                |
-| sys                   |
-| world                 |
-+-----------------------+
-9 rows in set (1.69 sec)
+mysql> DESC Address_Book;
++-------------+--------------+------+-----+---------+-------+
+| Field       | Type         | Null | Key | Default | Extra |
++-------------+--------------+------+-----+---------+-------+
+| firstName   | varchar(255) | YES  |     | NULL    |       |
+| lastName    | varchar(255) | YES  |     | NULL    |       |
+| address     | varchar(255) | YES  |     | NULL    |       |
+| city        | varchar(255) | YES  |     | NULL    |       |
+| state       | varchar(255) | YES  |     | NULL    |       |
+| zip         | int          | YES  |     | NULL    |       |
+| phoneNumber | int          | YES  |     | NULL    |       |
+| email       | varchar(255) | YES  |     | NULL    |       |
++-------------+--------------+------+-----+---------+-------+
 
